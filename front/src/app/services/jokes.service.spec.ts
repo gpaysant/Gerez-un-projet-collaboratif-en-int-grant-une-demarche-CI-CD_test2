4aa9bede1658ff39fed71cd4a382bd1b85ca183e
Gerald P <thegege2005@yahoo.fr>
2024-07-10 09:52:52 +0200

    [FRONT] add test to check if test coverage is been executed correctly

--- front/src/app/services/jokes.service.spec.ts
+++ front/src/app/services/jokes.service.spec.ts
@@ -2,9 +2,14 @@ import { TestBed } from '@angular/core/testing';
 
 import { JokesService } from './jokes.service';
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import { Joke } from '../model/joke.model';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+
 
 describe('JokesService', () => {
   let service: JokesService;
+  let httpClient: HttpClient;
 
   beforeEach(() => {
       TestBed.configureTestingModule({
@@ -12,10 +17,29 @@ describe('JokesService', () => {
         providers: [JokesService]
       });
       service = TestBed.get(JokesService);
+      httpClient = TestBed.inject(HttpClient);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should give one joke', () => {
+    const jokes:Joke = 
+      {
+        joke: 'what time is it?',
+        response: 'it'
+      }
+    ;
+
+    const httpGetSpy = spyOn(httpClient, 'get')
+      .and.returnValue(of(jokes)); 
+    service.getRandomJoke();
+    expect(httpGetSpy).toHaveBeenCalled();
+    service.joke$().subscribe({
+      next: response => expect(response).toEqual(jokes)
+    });
+
   });
 
 });
